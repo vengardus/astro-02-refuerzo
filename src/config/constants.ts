dace178202a70ea5b928e719f869cebbb18d81af
Ismytv <ismytv@gmail.com>
2024-09-21 19:58:38 -0500

    feat:NavBar2N navbar a 2 niveles

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,43 @@
+import type { Navbar } from "@/interfaces/shared/Navbar";
+
 export const siteInfo = {
-    title: "My Site",
-    description: "This is my site",
-}
+  title: "My Site",
+  description: "This is my site",
+};
+
+export const navBarOptions: Navbar[] = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Test",
+    path: "/test",
+  },
+  {
+    name: "Islas",
+    path: "",
+    options: [
+      {
+        name: "Isla 1",
+        path: "/isla1",
+      },
+      {
+        name: "Isla 2",
+        path: "/isla2",
+      },
+    ],
+  },
+  {
+    name: "Paises",
+    path: "/paises",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+];
